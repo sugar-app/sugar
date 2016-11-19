03f41ac3457c36545995f272497e059ee4875e91
Vladimir Mihai <vladimir.iacob@everymatrix.com>
2016-11-19 14:53:30 +0200

    add for prototype for adding pacient

--- app/src/pages/doctor/home/add-pacient/index.ts
+++ app/src/pages/doctor/home/add-pacient/index.ts
@@ -1,11 +1,14 @@
 import { NavController } from 'ionic-angular';
 import { Component } from '@angular/core';
 
+import { Pacient } from '../../../../interfaces/pacient';
+
 @Component({
   selector: 'add-pacient',
-  template: 'add-pacient.html'
+  templateUrl: 'add-pacient.html'
 })
 export class AddPacientPage {
+  public pacient: Pacient;
 
   constructor(public navCtrl: NavController) {
 
