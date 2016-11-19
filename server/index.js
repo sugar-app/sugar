2efb99ed65771a3d88b50fcaff3d97bfd6a56636
Alex Bularca <alexandru.bularca@everymatrix.com>
2016-11-19 12:14:48 +0200

    configure travis deploy to heroku

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World')
 })
 
 app.listen(port, function () {
