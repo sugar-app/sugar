cbb5c0c24e08530fde91c2df5c3a0641e90567c9
Alex Bularca <alexandru.bularca@everymatrix.com>
2016-11-19 12:11:09 +0200

    add missing index.js

---
+++ server/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port', port)
+})
