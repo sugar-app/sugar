6ffd18068e822fb5a4592a2e8f4104752adc1d6c
Bogdan Maxim <me@bogdanmaxim.ro>
2016-11-20 11:45:02 +0200

    blood glucose level chart

--- app/src/pages/pacient/graphics/graphics.ts
+++ app/src/pages/pacient/graphics/graphics.ts
@@ -8,11 +8,34 @@ import { NavController } from 'ionic-angular';
 })
 export class GraphicsPage {
   chartData = {
-    title: 'Example chart',
+    title: 'Blood glucose levels',
+    subTitle: 'This week',
+    xAxis: {
+            title: { text: 'Day of week' },
+            categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        },
+    yAxis: {
+            title: {
+                text: 'mg/dl'
+            },
+            plotBands: [{ // Light air
+                from: 90,
+                to: 130,
+                color: 'rgba(68, 170, 213, 0.1)',
+                label: {
+                    text: 'Normal values',
+                    style: {
+                        color: '#606060'
+                    }
+                }
+            }]
+        },
     series: [{
-      data: [29.9, 71.5, 106.4, 129.2]
+      name: 'Blood glucose level',
+      data: [170, 124, 200, 150, 130, 110, 90]
     }],
-    spacingRight: 10
+    spacingRight: 10,
+    type: 'spline'
   };
 
   constructor(public navCtrl: NavController) {
